7efece3a34ce820ed670b786d05064f44b5c1a73
Meri Takahashi <meri@dot1x6134.14-192-19.wlan.ms.dendai.ac.jp>
2018-02-10 12:25:11 +0900

    MONGO

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
 
+const MONGO_URI = process.env.MONGODB_URI;
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+  console.log(MONGO_URI);
 });
 
 module.exports = router;
